1850f1624a54f47a23f636f65ceffd5142838556
ivanhulak <g.ivan.gulak17@gmail.com>
2022-12-26 15:03:05 +0200

    Regenerate API key

--- src/DAL/api.ts
+++ src/DAL/api.ts
@@ -4,7 +4,7 @@ export const instance = axios.default.create({
     baseURL: 'https://social-network.samuraijs.com/api/1.0/',
     withCredentials: true,
     headers: {
-        'API-KEY': '3ce6ad81-6d64-4f4f-af4d-4c875642d779'
+        'API-KEY': 'ef6f20ec-689e-4598-ac27-ae5e93861aa4'
     }
 })
 
